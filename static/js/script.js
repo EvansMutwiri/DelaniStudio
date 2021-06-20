a3c9f4ed072ab4d4259e2dbadf674b948e196c45
EvansMutwiri <evans.mwiti@student.moringaschool.com>
2021-06-20 08:41:23 +0300

    Add thankyou alert

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,3 @@
-$(document).ready(function() {
-    $("img[src='assets/services_icons/design_icon.png']").click(function() {
-        $("<p>Our design practice offers a full range of services <br> including brand strategy, interaction, and visual design <br> </p>").appendTo(".design-pop");
-
-    });
-});
+function thankYou() {
+    alert("Thank You we have received your message.");
+  };
